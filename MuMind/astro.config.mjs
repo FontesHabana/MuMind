c1acfb1c21a5127bb3936e7c11d76fb242b975e0
Javier Fontes Basabe <142742715+FontesHabana@users.noreply.github.com>
2025-08-09 18:12:38 -0400

    Update astro.config.mjs

--- MuMind/astro.config.mjs
+++ MuMind/astro.config.mjs
@@ -4,7 +4,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-
+site: 'https://FontesHabana.github.io', 
+  // Reemplaza <TU_REPOSITORIO> con el nombre de tu repositorio
+  base: '/MuMind/',
 // https://astro.build/config
 export default defineConfig({
   vite: {
