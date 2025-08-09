a07d9939349f2f6aabc092191907c56885b193d4
Javier Fontes <javierfontbas@gmail.com>
2025-08-09 18:27:25 -0400

    Update config

--- MuMind/astro.config.mjs
+++ MuMind/astro.config.mjs
@@ -4,14 +4,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-site: 'https://FontesHabana.github.io', 
-  // Reemplaza <TU_REPOSITORIO> con el nombre de tu repositorio
-  base: '/MuMind/',
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
+  site: 'https://FontesHabana.github.io', 
+  // Reemplaza <TU_REPOSITORIO> con el nombre de tu repositorio
+  base: '/MuMind/',
   integrations: [react()]
 });
